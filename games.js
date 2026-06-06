7c17c9dfb15938fb4181020f8bfdd091310f5592
Lane <121688300+AlexLevel0@users.noreply.github.com>
2026-06-06 21:38:24 +0900

    Update games.js

--- games.js
+++ games.js
@@ -1,18 +1,18 @@
 // games.js
 
+// games.js
+
 export const GAMES = [
   {
     id: "2048",
     title: "2048",
     description: "数字を合体させて高得点を目指すパズル",
     script: "./games/game2048.js"
+  },
+  {
+    id: "taprush",
+    title: "Tap Rush",
+    description: "25秒以内に高得点マスを連打する反射ゲーム",
+    script: "./games/taprush.js"
   }
-
-  // 追加例
-  // {
-  //   id: "maze",
-  //   title: "迷路",
-  //   description: "ゴールまでの速さを競うゲーム",
-  //   script: "./games/maze.js"
-  // }
 ];
