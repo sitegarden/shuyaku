dba186d29a974e1f291bb69a3085914a23d08eac
Lane <121688300+AlexLevel0@users.noreply.github.com>
2026-06-11 20:16:11 +0900

    games.js を更新

--- games/games.js
+++ games/games.js
@@ -5,24 +5,24 @@ export const GAMES = [
     id: "2048",
     title: "2048",
     description: "数字を合体させて高得点を目指すパズル",
-    script: "./games/game2048.js"
+    script: "./game2048.js"
   },
   {
     id: "taprush",
     title: "Tap Rush",
     description: "30秒以内に高得点マスを連打する反射ゲーム",
-    script: "./games/taprush.js"
+    script: "./taprush.js"
   },
   {
     id: "sunvader",
     title: "sunvader",
     description: "太陽を集めてモンスターを避けるランゲーム",
-    script: "./games/sunvader.js"
+    script: "./sunvader.js"
   },
   {
     id: "memorychain",
     title: "Memory Chain",
     description: "光った順番を覚えてタッチする記憶ゲーム",
-    script: "./games/memorychain.js"
+    script: "./memorychain.js"
   }
 ];
